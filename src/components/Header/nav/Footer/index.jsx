3d486e540a9add39aca66f717125bc3166aad118
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-01-29 23:59:58 +0400

    first commit

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,39 @@
+import styles from './style.module.scss';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Rounded from '../../../../common/RoundedButton';
+import Magnetic from '../../../../common/Magnetic';
+import { usePathname } from 'next/navigation';
+
+export default function index() {
+  return (
+    <>
+    <div className={styles.footer}>
+      <Magnetic>
+        <div className={styles.el}>
+        <a href='' target='_blank'>LinkedIn</a>
+        <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
+      <Magnetic>
+        <div className={styles.el}>
+        <a href='' target='_blank'>GitHub</a>
+        <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
+      <Magnetic>
+        <div className={styles.el}>
+        <a href='' target='_blank'>Twitter</a>
+        <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
+      <Magnetic>
+        <div className={styles.el}>
+        <a href='https://drive.google.com/file/d/18StCZFuKOo7RkzZxnvuk93OuQuct0Svi/view?usp=sharing' target='_blank'>Resume</a>
+        <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
+    </div>
+    </>
+  )
+}
