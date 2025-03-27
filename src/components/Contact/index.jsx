9c2c668e515ad0c14640669107ccd65c9adae2f6
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-03-27 23:58:09 +0400

    contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,81 +1,122 @@
 import styles from './styles.module.scss';
-import Image from 'next/image';
 import { useRef } from 'react';
-import { useScroll, motion, useTransform, useSpring } from 'framer-motion';
+import { useScroll, motion, useTransform } from 'framer-motion';
 import Magnetic from '../../common/Magnetic';
 import Rounded from '../../common/RoundedButton';
+import { FaLinkedin, FaDiscord, FaTwitter, FaWhatsapp, FaEnvelope, FaGithub, FaFileAlt } from 'react-icons/fa';
 
-export default function index() {
+export default function Contact() {
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
         target: container,
         offset: ["start end", "end end"]
-    })
-    const x = useTransform(scrollYProgress, [0, 1], [0, 100])
-    const y = useTransform(scrollYProgress, [0, 1], [-500, 0])
-    const rotate = useTransform(scrollYProgress, [0, 1], [120, 90])
+    });
+    
+    const x = useTransform(scrollYProgress, [0, 1], [0, 100]);
+    const y = useTransform(scrollYProgress, [0, 1], [-500, 0]);
+    const rotate = useTransform(scrollYProgress, [0, 1], [120, 90]);
+    
     return (
         <motion.div style={{y}} ref={container} className={styles.contact} id='contact'>
             <div className={styles.body}>
                 <div className={styles.title}>
                     <span>
-                        <div className={styles.imageContainer}>
-                            <Image 
-                            fill={true}
-                            alt={"image"}
-                            src={`/images/shuriken1.png`}
+                        <div className={styles.decorativeElement}>
+                            <motion.div 
+                                className={styles.circle}
+                                animate={{ 
+                                    scale: [.3, .3, .3],
+                                    opacity: [0.7, 1, 0.7],
+                                }}
+                            />
+                            <motion.div 
+                                className={styles.circle}
+                                animate={{ 
+                                    scale: [1, .5, 1],
+                                    opacity: [0.7, 1, 0.7],
+                                }}
+                                transition={{
+                                    duration: 2,
+                                    repeat: Infinity,
+                                    ease: "easeInOut"
+                                }}
+                            />
+                            <motion.div 
+                                className={styles.circle}
+                                animate={{ 
+                                    scale: [1.2, .5, 1.2],
+                                    opacity: [0.7, 1, 0.7],
+                                }}
+                                transition={{
+                                    duration: 3,
+                                    repeat: Infinity,
+                                    ease: "easeInOut"
+                                }}
                             />
                         </div>
-                        <h2>Let's work</h2>
+                        <h2>Let's Work</h2>
+                        <h2>Together</h2>
                     </span>
-                    <h2>together</h2>
                     <motion.div style={{x}} className={styles.buttonContainer}>
                         <Rounded className={styles.rounded}>
                             <p>Get in touch</p>
                         </Rounded>
                     </motion.div>
-                    <motion.svg style={{rotate, scale: 2}} width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
-                    </motion.svg>
                 </div>
+                
                 <div className={styles.nav}>
-                        <div className={styles.button}>
-                            <p>abdulmajeed301100@gmail.com</p>
-                        </div>
-                        <div className={styles.button}>
-                            <p>+971-58-804-6607</p>
-                        </div>
+                    <a href="mailto:abdulmajeed301100@gmail.com" className={styles.button}>
+                        <FaEnvelope className={styles.icon} />
+                        <p>abdulmajeed301100@gmail.com</p>
+                    </a>
+                    <a href="https://wa.me/971588046607" className={styles.button}>
+                        <FaWhatsapp className={styles.icon} />
+                        <p>+971-58-804-6607</p>
+                    </a>
                 </div>
+                
                 <div className={styles.info}>
                     <div>
                         <span>
-                            <h3>Version</h3>
+                            <h3>Portfolio</h3>
                             <p>2025 © Edition</p>
                         </span>
                         <span>
-                            <h3>Version</h3>
-                            <p>hey</p>
+                            <h3>Location</h3>
+                            <p>Dubai, UAE</p>
                         </span>
                     </div>
-                    <div>
-                        <span>
-                            <h3>socials</h3>
+                    <div className={styles.socials}>
+                        <div className={styles.socialLinks}>
                             <Magnetic>
-                                <p>GitHub</p>
+                                <a href="https://github.com/abdul-majeed-khan" target="_blank" rel="noopener noreferrer" className={styles.socialIcon}>
+                                    <FaGithub size={22} />
+                                </a>
                             </Magnetic>
-                        </span>
-                        <Magnetic>
-                            <p>Twitter</p>
-                        </Magnetic>
-                        <Magnetic>
-                            <p>LinkedIn</p>
-                        </Magnetic>
-                        <Magnetic>
-                            <p>Resume</p>
-                        </Magnetic>
+                            <Magnetic>
+                                <a href="https://www.linkedin.com/in/abdul-majeed-khan/" target="_blank" rel="noopener noreferrer" className={styles.socialIcon}>
+                                    <FaLinkedin size={22} />
+                                </a>
+                            </Magnetic>
+                            <Magnetic>
+                                <a href="https://x.com/Majeed_00_" target="_blank" rel="noopener noreferrer" className={styles.socialIcon}>
+                                    <FaTwitter size={22} />
+                                </a>
+                            </Magnetic>
+                            <Magnetic>
+                                <a href="https://discord.com/users/abdulmajeed1572" target="_blank" rel="noopener noreferrer" className={styles.socialIcon}>
+                                    <FaDiscord size={22} />
+                                </a>
+                            </Magnetic>
+                            <Magnetic>
+                                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className={styles.socialIcon}>
+                                    <FaFileAlt size={22} />
+                                </a>
+                            </Magnetic>
+                        </div>
                     </div>
                 </div>
             </div>
         </motion.div>
-    )
+    );
 }
