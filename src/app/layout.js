953a056efb51f0a1d27f42ac562102aca89b933a
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-03-28 02:17:24 +0400

    removed cursor

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css"
 import Header from '../components/Header';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import ClientCursor from './ClientCursor';
 
 
 export const metadata = {
@@ -18,7 +17,6 @@ export default function RootLayout({ children }) {
         <meta name="google-site-verification" content="P2_1zWxkE0R-QmgUGw4dGpmHqVlIO0X-SaoFDdH-ciM" />
       </head>
       <body >
-        <ClientCursor />
         <Header />
         <SpeedInsights/>
         <Analytics/>
