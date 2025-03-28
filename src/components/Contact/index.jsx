2a5cbd157e50538103422edeb2c4f0d4d3ded96f
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-03-28 16:25:57 +0400

    reduce y scroll

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
     });
     
     const x = useTransform(scrollYProgress, [0, 1], [0, 100]);
-    const y = useTransform(scrollYProgress, [0, 1], [-500, 0]);
+    const y = useTransform(scrollYProgress, [0, 1], [-100, 0]);
     const rotate = useTransform(scrollYProgress, [0, 1], [120, 90]);
     
     return (
