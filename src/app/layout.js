f67901b654ea299c2dab73596637a38f54efdac2
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-04-08 13:11:27 +0400

    GA added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css"
 import Header from '../components/Header';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script"
 
 
 export const metadata = {
@@ -12,14 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="StKnFVMdxYtC2pDJPSr7oQjfKmXrAl1UXRLLkokFj8o" />
-        <meta name="google-site-verification" content="P2_1zWxkE0R-QmgUGw4dGpmHqVlIO0X-SaoFDdH-ciM" />
-      </head>
       <body >
         <Header />
-        <SpeedInsights/>
-        <Analytics/>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-W5TBNJTXLK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W5TBNJTXLK');
+          `}
+        </Script>
         {children}
       </body>
     </html>
